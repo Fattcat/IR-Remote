2ebe2ae05da9017debd5578cb52268d8281ad5df
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-08-25 16:26:09 +0200

    Update D1-Mini-esp8266-IR-Receive.ino

--- D1-Mini-esp8266-IR-Receive.ino
+++ D1-Mini-esp8266-IR-Receive.ino
@@ -1,2 +1,33 @@
 // Receive and show IR Signal Data in Serial Monitor
 
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+
+const uint16_t kRecvPin = D2;  // GPIO4 na D1 Mini
+
+// ---------------------------------
+// Connection
+// IR Receiver --> esp8266 D1 Mini
+// VCC --> 5V
+// GND --> GND
+// Pin --> D2
+// ---------------------------------
+
+IRrecv irrecv(kRecvPin);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(115200);
+  irrecv.enableIRIn();  // Inicializácia IR prijímača
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) 
+  {
+    Serial.print("Received IR code: ");
+    Serial.println(results.value, HEX);
+    irrecv.resume();  // Pokračovanie v prijímaní ďalších signálov
+  }
+}
